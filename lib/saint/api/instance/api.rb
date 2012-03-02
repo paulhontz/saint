c9b15aecafdb1c11071b507269d6d387aba02f13
slivu <slivuz@gmail.com>
2012-03-02 23:14:49 +0200

    adding back the abbility to use custom layouts

--- lib/saint/api/instance/api.rb
+++ lib/saint/api/instance/api.rb
@@ -12,6 +12,18 @@ module Saint
       @node.saint.after.each_pair { |a, p| @orm.after a, &p }
 
     end
+
+    def meta_title
+      @__meta_title__
+    end
+
+    def assets
+      @node.saint.render_assets
+    end
+
+    def menu
+      @node.saint.render_menu
+    end
     
     def dashboard
       @node.saint.render_dashboard @node_instance
